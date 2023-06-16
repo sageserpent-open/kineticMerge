78584da5e004a82117478cecf905f16a1607c645
Gerard Murphy <gjmurphy1@icloud.com>
2023-06-16 10:33:22 +0100

    Go back to implementing fake sections as case classes, this is in anticipation of splitting / merging sections.

--- src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
@@ -10,17 +10,26 @@ end CodeMotionAnalysisTest
 
 object CodeMotionAnalysisTest:
   case class FakeSources(textsByPath: Map[Int, String]) extends Sources[Int]:
+    override type Path = Int
+
+    case class SectionImplementation(
+        path: Path,
+        override val startOffset: Int,
+        override val width: Int
+    ) extends Section:
+      override def contents: String =
+        textsByPath(path).substring(startOffset, onePastEndOffset)
+    end SectionImplementation
+
     override def filesByPath: Map[Path, File] =
       textsByPath.map { case (path, text) =>
         path -> File(
           Vector(
-            new Section:
-              override def startOffset: Int = 0
-
-              override def width: Int = text.length
-
-              override def contents: String =
-                textsByPath(path).substring(startOffset, onePastEndOffset)
+            SectionImplementation(
+              path = path,
+              startOffset = 0,
+              width = text.length
+            )
           )
         )
       }
