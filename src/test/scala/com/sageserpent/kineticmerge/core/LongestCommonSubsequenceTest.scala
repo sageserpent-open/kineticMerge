693560d6e38601737cc2ea0e23965fb4f1a6d6c9
Gerard Murphy <gjmurphy1@icloud.com>
2023-07-12 21:04:11 +0100

    `LongestCommonSubsequenceTest` is now finessed to take into account partial agreements and still check differences into the bargain.

--- src/test/scala/com/sageserpent/kineticmerge/core/LongestCommonSubsequenceTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/LongestCommonSubsequenceTest.scala
@@ -91,8 +91,10 @@ class LongestCommonSubsequenceTest:
 
           extension (sequence: IndexedSeq[Contribution[Element]])
             private def verifyLongestCommonSubsequence(
-                elements: IndexedSeq[Element]
+                elements: IndexedSeq[Element], commonSubsequenceSize: Int
             ): Unit =
+              // Part #1 - verify the commonality - all sides should agree...
+
               val indexedCommonParts: IndexedSeq[(Int, Element)] =
                 sequence.zipWithIndex.collect:
                   case (common: Contribution.Common[Element], index) =>
@@ -104,19 +106,21 @@ class LongestCommonSubsequenceTest:
               val _ = commonSubsequence isSubsequenceOf testCase.left
               val _ = commonSubsequence isSubsequenceOf testCase.right
 
-              assert(commonSubsequence.size >= coreSize)
+              assert(commonSubsequence.size == commonSubsequenceSize)
+
+              // Part #2 - verify the length is maximal - anything else should cause a difference somewhere...
 
               def verifyDifference(viveLaDifférence: IndexedSeq[Element]): Unit =
                 if elements != testCase.base then
-                  viveLaDifférence isNotSubsequenceOf testCase.base
+                  val _ = viveLaDifférence isNotSubsequenceOf testCase.base
                 end  if
 
                 if elements != testCase.left then
-                    viveLaDifférence isNotSubsequenceOf testCase.left
+                    val _ = viveLaDifférence isNotSubsequenceOf testCase.left
                 end if
 
                 if elements != testCase.right then
-                    viveLaDifférence isNotSubsequenceOf testCase.right
+                    val _ = viveLaDifférence isNotSubsequenceOf testCase.right
                 end if
               end verifyDifference
               
@@ -138,19 +142,38 @@ class LongestCommonSubsequenceTest:
                 end if
               end verifyCommonLeftAndRight
 
-              def insert(contribution: Contribution[Element], contributionIndex: Int) =
+              def insert(contribution: Contribution[Element], contributionIndex: Int, background: IndexedSeq[(Int, Element)]) =
                 val (leadingCommonIndices, trailingCommonIndices) =
-                  indexedCommonParts.span { case (commonIndex, _) =>
+                  background.span { case (commonIndex, _) =>
                     contributionIndex > commonIndex
                   }
 
                 leadingCommonIndices.map(
                   _._2
                 ) ++ (contribution.element +: trailingCommonIndices.map(_._2))
+              end insert
 
-              // NASTY HACK: need to finesse the situation where a valid difference
-              // can match an out-of-place element on another side due to partial matches.
-              val forgetAboutValidatingDifferences = sequence.exists {
+              val indexedCommonPartsAndPartialAgreements: IndexedSeq[(Int, Element)] =
+                sequence.zipWithIndex.collect:
+                  case (common: Contribution.Common[Element], index) =>
+                    index -> common.element
+                  case (common: Contribution.CommonToLeftAndRightOnly[Element], index) =>
+                    index -> common.element
+                  case (common: Contribution.CommonToBaseAndLeftOnly[Element], index) =>
+                    index -> common.element
+                  case (common: Contribution.CommonToBaseAndRightOnly[Element], index) =>
+                    index -> common.element
+
+              // Partial agreements in their own right are no problem - they mop up corner
+              // cases where a straight assertion on differences would fail. What we have
+              // to worry about is when an element that is different in `sequence` wrt to
+              // other two sides matches further up or down on one of those sides because
+              // it is duplicated in more than one position on that side - when this
+              // happens, at least one of the duplicates will have been partially matched
+              // (think about it). Including the partial matches in with the common backbone
+              // refines the position of the differing element so we don't get spurious
+              // subsequence matches.
+              val havePartialAgreementsThatNeedToBeTreatedAsCommon = sequence.exists {
                 case _: Contribution.CommonToLeftAndRightOnly[Element] => true
                 case _: Contribution.CommonToBaseAndLeftOnly[Element] => true
                 case _: Contribution.CommonToBaseAndRightOnly[Element] => true
@@ -160,19 +183,23 @@ class LongestCommonSubsequenceTest:
                 case (
                   difference: Contribution.Difference[Element],
                   index
-                  ) if !forgetAboutValidatingDifferences => verifyDifference(insert(difference, index))
+                  ) if havePartialAgreementsThatNeedToBeTreatedAsCommon => verifyDifference(insert(difference, index, indexedCommonPartsAndPartialAgreements))
+                case (
+                  difference: Contribution.Difference[Element],
+                  index
+                  ) if !havePartialAgreementsThatNeedToBeTreatedAsCommon => verifyDifference(insert(difference, index, indexedCommonParts))
                 case (
                   difference: Contribution.CommonToBaseAndLeftOnly[Element],
                   index
-                  ) => verifyCommonBaseAndLeft(insert(difference, index))
+                  ) => verifyCommonBaseAndLeft(insert(difference, index, indexedCommonParts))
                 case (
                   difference: Contribution.CommonToBaseAndRightOnly[Element],
                   index
-                  ) => verifyCommonBaseAndRight(insert(difference, index))
+                  ) => verifyCommonBaseAndRight(insert(difference, index, indexedCommonParts))
                 case (
                   difference: Contribution.CommonToLeftAndRightOnly[Element],
                   index
-                  ) => verifyCommonLeftAndRight(insert(difference, index))
+                  ) => verifyCommonLeftAndRight(insert(difference, index, indexedCommonParts))
                 case _ =>
               }
           end extension
@@ -191,12 +218,9 @@ class LongestCommonSubsequenceTest:
           // same size. All the core sequence does is to guarantee that there
           // will be *some* common subsequence.
           // NASTY HACK: placate IntelliJ with these underscore bindings.
-          val _ =
-            base verifyLongestCommonSubsequence testCase.base
-          val _ =
-            left verifyLongestCommonSubsequence testCase.left
-          val _ =
-            right verifyLongestCommonSubsequence testCase.right
+          val _ = base.verifyLongestCommonSubsequence(testCase.base, commonSubsequenceSize)
+          val _ = left.verifyLongestCommonSubsequence(testCase.left, commonSubsequenceSize)
+          val _ = right verifyLongestCommonSubsequence(testCase.right, commonSubsequenceSize)
 
           // NOTE: The reason for the lower bound on size (rather than strict
           // equality) is because the interleaves for the base, left and right
