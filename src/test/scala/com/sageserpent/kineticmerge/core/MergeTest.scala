a833b4053498f6832439251079c446f6ff483cbc
Gerard Murphy <gjmurphy1@icloud.com>
2023-08-25 17:09:11 +0100

    Cutover bug reproduction test to the latest failure...

--- src/test/scala/com/sageserpent/kineticmerge/core/MergeTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/MergeTest.scala
@@ -22,12 +22,78 @@ class MergeTest:
   @Test
   def bugReproduction(): Unit =
     val testCase = MergeTestCase(
-      base = Vector(),
-      left = Vector(FakeSection(zeroRelativeLabel = 2)),
-      right = Vector(),
-      matchesBySection = Map(),
-      expectedMerge = FullyMerged(sections = Vector(FakeSection(zeroRelativeLabel = 2))),
-      moves = Vector(Move.LeftInsertion)
+      base = Vector(FakeSection(zeroRelativeLabel = 2), FakeSection(zeroRelativeLabel = 17)),
+      left = Vector(
+        FakeSection(zeroRelativeLabel = 3),
+        FakeSection(zeroRelativeLabel = 6),
+        FakeSection(zeroRelativeLabel = 9),
+        FakeSection(zeroRelativeLabel = 13),
+        FakeSection(zeroRelativeLabel = 21)
+      ),
+      right = Vector(
+        FakeSection(zeroRelativeLabel = 10),
+        FakeSection(zeroRelativeLabel = 14),
+        FakeSection(zeroRelativeLabel = 18),
+        FakeSection(zeroRelativeLabel = 22)
+      ),
+      matchesBySection = Map(
+        FakeSection(zeroRelativeLabel = 10) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 9),
+          rightSection = FakeSection(zeroRelativeLabel = 10)
+        ),
+        FakeSection(zeroRelativeLabel = 9) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 9),
+          rightSection = FakeSection(zeroRelativeLabel = 10)
+        ),
+        FakeSection(zeroRelativeLabel = 21) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 21),
+          rightSection = FakeSection(zeroRelativeLabel = 22)
+        ),
+        FakeSection(zeroRelativeLabel = 3) -> Match.BaseAndLeft(
+          baseSection = FakeSection(zeroRelativeLabel = 2),
+          leftSection = FakeSection(zeroRelativeLabel = 3)
+        ),
+        FakeSection(zeroRelativeLabel = 13) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 13),
+          rightSection = FakeSection(zeroRelativeLabel = 14)
+        ),
+        FakeSection(zeroRelativeLabel = 2) -> Match.BaseAndLeft(
+          baseSection = FakeSection(zeroRelativeLabel = 2),
+          leftSection = FakeSection(zeroRelativeLabel = 3)
+        ),
+        FakeSection(zeroRelativeLabel = 22) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 21),
+          rightSection = FakeSection(zeroRelativeLabel = 22)
+        ),
+        FakeSection(zeroRelativeLabel = 17) -> Match.BaseAndRight(
+          baseSection = FakeSection(zeroRelativeLabel = 17),
+          rightSection = FakeSection(zeroRelativeLabel = 18)
+        ),
+        FakeSection(zeroRelativeLabel = 14) -> Match.LeftAndRight(
+          leftSection = FakeSection(zeroRelativeLabel = 13),
+          rightSection = FakeSection(zeroRelativeLabel = 14)
+        ),
+        FakeSection(zeroRelativeLabel = 18) -> Match.BaseAndRight(
+          baseSection = FakeSection(zeroRelativeLabel = 17),
+          rightSection = FakeSection(zeroRelativeLabel = 18)
+        )
+      ),
+      expectedMerge = FullyMerged(
+        sections = Vector(
+          FakeSection(zeroRelativeLabel = 6),
+          FakeSection(zeroRelativeLabel = 9),
+          FakeSection(zeroRelativeLabel = 13),
+          FakeSection(zeroRelativeLabel = 21)
+        )
+      ),
+      moves = Vector(
+        Move.RightDeletion,
+        Move.LeftInsertion,
+        Move.CoincidentInsertion,
+        Move.CoincidentInsertion,
+        Move.LeftDeletion,
+        Move.CoincidentInsertion
+      )
     )
 
     pprint.pprintln(testCase)
