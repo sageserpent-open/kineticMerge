7c0200ef6822096a577a194ad5d7228d49e397ff
Gerard Murphy <gjmurphy1@icloud.com>
2023-09-21 11:13:04 +0100

    Find the blob id and content for the modified and added files. Also rename for clarity.

--- src/main/scala/com/sageserpent/kineticmerge/Main.scala
+++ src/main/scala/com/sageserpent/kineticmerge/Main.scala
@@ -8,43 +8,64 @@ import scala.sys.process.*
 import scala.util.Try
 
 object Main:
+  private type BlobId = String
+
+  private type CommitIdOrBranchName = String
+  private val whitespaceRun = "\\s+"
+
+  private val ourBranchHead = "HEAD"
+
+  extension [Payload](fallible: Try[Payload])
+    private def labelExceptionWith(label: String): Either[String, Payload] =
+      fallible.toEither.left.map(_ => label)
+  end extension
+
   def main(args: Array[String]): Unit =
+    // NOTE: the use of Git below is based on spike work on MacOS - the version
+    // of Git shipped tends to be a *long* way behind the latest release, so the
+    // latest and greatest versions of commands are not always available. At
+    // time of writing, Mac OS Ventura 13.5.2 ships Git 2.24.3, contrast with
+    // Git 2.42.0 being the latest stable release.
+
     args match
-      case Array(theirBranchHead) =>
+      case Array(theirBranchHead: CommitIdOrBranchName) =>
         val workflow = for
-          _ <- Try { "git --version" !! }.toEither.left.map(_ =>
-            "Git is not available."
-          )
+          _ <- Try { "git --version" !! }
+            .labelExceptionWith(label = "Git is not available.")
+
           workingTree <- Try {
             "git rev-parse --show-toplevel" !!
-          }.toEither.left.map(_ =>
+          }.labelExceptionWith(label =
             "The current working directory is not part of a Git working tree."
           )
-          _ <- Try { "git diff-index --exit-code HEAD" !! }.toEither.left.map(
-            _ => "There are uncommitted changes prior to commencing the merge."
-          )
-          workingTreePath <- Try { Path.of(workingTree) }.toEither.left.map(_ =>
-            s"Unexpected error: working tree reported by Git: $workingTree is not a valid path."
-          )
+
+          _ <- Try { s"git diff-index --exit-code $ourBranchHead" !! }
+            .labelExceptionWith(label =
+              "There are uncommitted changes prior to commencing the merge."
+            )
+
+          workingTreePath <- Try { Path.of(workingTree) }
+            .labelExceptionWith(label =
+              s"Unexpected error: working tree reported by Git: $workingTree is not a valid path."
+            )
+
           bestAncestorCommit <- Try {
-            s"git merge-base HEAD $theirBranchHead" !!
-          }.toEither.left.map(_ =>
+            s"git merge-base $ourBranchHead $theirBranchHead" !!
+          }.labelExceptionWith(label =
             s"$theirBranchHead is not a valid branch or commit."
           )
+
           ourChanges <- Try {
-            s"git diff --no-renames --name-status $bestAncestorCommit HEAD".lazyLines.toList
-          }.toEither.left
-            .map(_ =>
-              s"Can't determine changes made on our branch since ancestor commit $bestAncestorCommit"
-            )
-            .flatMap(_.traverse(pathChangeFor))
+            s"git diff --no-renames --name-status $bestAncestorCommit $ourBranchHead".lazyLines.toList
+          }.labelExceptionWith(label =
+            s"Can't determine changes made on our branch since ancestor commit $bestAncestorCommit"
+          ).flatMap(_.traverse(pathChangeFor(ourBranchHead)))
+
           theirChanges <- Try {
             s"git diff --no-renames --name-status $bestAncestorCommit $theirBranchHead".lazyLines.toList
-          }.toEither.left
-            .map(_ =>
-              s"Can't determine changes made on their branch $theirBranchHead since ancestor commit $bestAncestorCommit"
-            )
-            .flatMap(_.traverse(pathChangeFor))
+          }.labelExceptionWith(label =
+            s"Can't determine changes made on their branch $theirBranchHead since ancestor commit $bestAncestorCommit"
+          ).flatMap(_.traverse(pathChangeFor(theirBranchHead)))
         yield (ourChanges, theirChanges)
 
         workflow.fold(
@@ -65,19 +86,47 @@ object Main:
         )
         System.exit(3)
 
-  private def pathChangeFor(line: String): Either[String, (Path, Changed)] =
+  private def pathChangeFor(
+      commitIdOrBranchName: CommitIdOrBranchName
+  )(line: String): Either[String, (Path, Changed)] =
     Try {
-      line.split("\\s+") match
-        case Array("M", path) => Path.of(path) -> Changed.Modified
-        case Array("A", path) => Path.of(path) -> Changed.Added
-        case Array("D", path) => Path.of(path) -> Changed.Deleted
-    }.toEither.left.map(_ =>
+      line.split(whitespaceRun) match
+        case Array("M", changedFile) =>
+          val path = Path.of(changedFile)
+          path -> blobAndContentFor(commitIdOrBranchName)(path).map(
+            Changed.Modified.apply.tupled
+          )
+        case Array("A", changedFile) =>
+          val path = Path.of(changedFile)
+          path -> blobAndContentFor(commitIdOrBranchName)(path).map(
+            Changed.Added.apply.tupled
+          )
+        case Array("D", path) =>
+          Path.of(path) -> Right(Changed.Deleted)
+    }.labelExceptionWith(label =
       s"Unexpected error - can't parse changes reported by Git: $line"
+    ).flatMap { case (path, changed) => changed.map(path -> _) }
+
+  private def blobAndContentFor(commitIdOrBranchName: CommitIdOrBranchName)(
+      path: Path
+  ): Either[String, (BlobId, String)] =
+    Try {
+      val line = s"git ls-tree $commitIdOrBranchName $path" !!
+
+      line.split(whitespaceRun) match
+        case Array(_, _, blobId, _) =>
+          val content = s"git cat-file blob $blobId" !!
+
+          blobId -> content
+      end match
+    }.labelExceptionWith(label =
+      s"Unexpected error - can't determine blob id for path: $path in commit or branch: $commitIdOrBranchName."
     )
+  end blobAndContentFor
 
   private enum Changed:
-    case Modified
-    case Added
+    case Modified(blobId: BlobId, content: String)
+    case Added(blobId: BlobId, content: String)
     case Deleted
   end Changed
 
--- src/main/scala/com/sageserpent/kineticmerge/core/Section.scala
+++ src/main/scala/com/sageserpent/kineticmerge/core/Section.scala
@@ -1,9 +1,9 @@
 package com.sageserpent.kineticmerge.core
 
 /** A [[Section]] covers some part of a [[File]], and knows how to render the
-  * contents.
+  * content.
   *
-  * At some point, it is likely that the actual contents will be abstracted over
+  * At some point, it is likely that the actual content will be abstracted over
   * too, so a run of whitespace can be condensed into a single placeholder to
   * allow whitespace-insensitive comparison of sources. For now though, we just
   * use a plain [[String]].
@@ -19,5 +19,5 @@ trait Section:
 
   def onePastEndOffset: Int = startOffset + size
 
-  def contents: String
+  def content: String
 end Section
