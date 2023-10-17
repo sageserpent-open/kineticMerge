ef92ded7d0885b6246b10307213ab71484fdec6a
Gerard Murphy <gjmurphy1@icloud.com>
2023-10-17 15:05:28 +0100

    Correct typo and add a test for significance of literal whitespace in a string literal. It passes, checked by debugging and inspecting assertions at runtime.

--- src/test/scala/com/sageserpent/kineticmerge/core/TokenTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/TokenTest.scala
@@ -2,12 +2,17 @@ package com.sageserpent.kineticmerge.core
 
 import com.sageserpent.kineticmerge.core.ExpectyFlavouredAssert.assert
 import com.sageserpent.kineticmerge.core.Token.{Significant, tokens}
-import com.sageserpent.kineticmerge.core.TokenTest.checkTokenization
+import com.sageserpent.kineticmerge.core.TokenTest.{checkTokenization, stringLiteral}
 import org.apache.commons.text.StringEscapeUtils
 import org.junit.jupiter.api.Test
 import pprint.pprintln
 
 object TokenTest:
+  val stringLiteral =
+    // NOTE: only the *body* of the string literal is escaped, so we have to
+    // add the enclosing quotation.
+    s"\"${StringEscapeUtils.escapeJava("This is a:\nstring\tliteral")}\""
+
   def checkTokenization(prose: String): Unit =
     val proseTokens = tokens(prose)
     pprintln(proseTokens)
@@ -17,26 +22,40 @@ end TokenTest
 
 class TokenTest extends ProseExamples:
   @Test
-  def tokenization(): Unit =
+  def tokenizationCoversTheText(): Unit =
     checkTokenization(wordsworth)
     checkTokenization(jobsworth)
     checkTokenization(emsworth)
     checkTokenization(scalaStuff)
-
-  end tokenization
+  end tokenizationCoversTheText
 
   @Test
-  def aStringLiteralsIsTreatedAsASingleToken(): Unit =
-    val literal =
-      // NOTE: only the *body* of the string literal is escaped, so we have to
-      // add the enclosing quotation.
-      s"\"${StringEscapeUtils.escapeJava("This is a:\nstring\tliteral")}\""
-
-    val prose = s"Not a string literal...$literal, but again, this isn't."
+  def aStringLiteralIsTreatedAsASingleToken(): Unit =
+    val prose = s"Not a string literal...$stringLiteral, but again, this isn't."
 
     checkTokenization(prose)
 
-    assert(tokens(prose).get.contains(Significant(literal)))
-  end aStringLiteralsIsTreatedAsASingleToken
+    assert(tokens(prose).get.contains(Significant(stringLiteral)))
+  end aStringLiteralIsTreatedAsASingleToken
+
+  @Test
+  def aStringLiteralHasSignificantLiteralWhitespace(): Unit =
+    val stringLiteralWithMangledWhitespace = stringLiteral.flatMap {
+      case space @ ' ' =>
+        s"$space$space" // Double the literal whitespace as a person would type it in.
+      case character => s"$character"
+    }
+
+    val prose =
+      s"Not a string literal...$stringLiteralWithMangledWhitespace, but again, this isn't."
+
+    assume(
+      tokens(prose).get.contains(
+        Significant(stringLiteralWithMangledWhitespace)
+      )
+    )
+
+    assert(!tokens(prose).get.contains(Significant(stringLiteral)))
+  end aStringLiteralHasSignificantLiteralWhitespace
 
 end TokenTest
