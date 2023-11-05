ee424fbcbe3d4748daae88c03520e899dfbb13e5
Gerard Murphy <gjmurphy1@icloud.com>
2023-11-05 13:58:17 +0000

    Add new test to `EvolutionTest` to show that an initially sorted phenotype will still yield a sorted phenotype. Test passes.

--- src/test/scala/com/sageserpent/kineticmerge/core/genetic/EvolutionTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/genetic/EvolutionTest.scala
@@ -5,29 +5,124 @@ import com.sageserpent.americium.Trials
 import com.sageserpent.americium.Trials.api as trialsApi
 import com.sageserpent.americium.junit5.*
 import com.sageserpent.kineticmerge.core.ExpectyFlavouredAssert.assert
+import com.sageserpent.kineticmerge.core.genetic.EvolutionTest.{*, given}
 import org.junit.jupiter.api.TestFactory
 
 import scala.collection.immutable.TreeSet
 import scala.util.Random
 
+object EvolutionTest:
+  type Phenotype  = Vector[Long]
+  type Chromosome = Vector[Int]
+  val ascendingValueSequences: Trials[Phenotype] =
+    trialsApi
+      .choose(0L until 9L)
+      .several[Vector[Long]]
+      .filter(_.nonEmpty)
+      .flatMap(increments =>
+        trialsApi
+          .longs(1L, 10L)
+          .map(lowest => increments.scanLeft(lowest)(_ + _))
+      )
+
+  given Order[Phenotype] =
+    Order.by(numberOfCorrectlyOrderedAdjacentElements)
+
+  def numberOfCorrectlyOrderedAdjacentElements(phenotype: Phenotype) =
+    phenotype.zip(phenotype.tail).count(_ <= _)
+  end numberOfCorrectlyOrderedAdjacentElements
+
+  def evolution(startingPhenotype: Phenotype) =
+    new Evolution[Chromosome, Phenotype]:
+      override def mutate(chromosome: Chromosome)(using
+          random: Random
+      ): Chromosome =
+        require(1 < chromosome.size)
+
+        if random.nextBoolean() then
+          // Swap a pair of indices...
+          val indexOne = random.nextInt(chromosome.size)
+
+          val indexTwo =
+            (indexOne + 1 + random.nextInt(
+              chromosome.size - 1
+            )) % chromosome.size
+
+          val withTwoIndicesSwapped = chromosome
+            .updated(indexOne, chromosome(indexTwo))
+            .updated(indexTwo, chromosome(indexOne))
+
+          withTwoIndicesSwapped
+        else
+          // Cyclic permutation...
+          val rotateLeftBy = 1 + random.nextInt(chromosome.size - 1)
+
+          val (prefix, suffix) = chromosome.splitAt(rotateLeftBy)
+
+          suffix ++ prefix
+        end if
+      end mutate
+
+      override def breed(
+          first: Chromosome,
+          second: Chromosome
+      )(using random: Random): Chromosome =
+        require(first.size == second.size)
+
+        val firstMapsToEven = random.nextBoolean()
+
+        val chosenUniqueValues = (first zip second).map {
+          (firstIndex, secondIndex) =>
+            val chooseTheFirst = random.nextBoolean()
+
+            if chooseTheFirst then
+              if firstMapsToEven then 2 * firstIndex
+              else 2 * firstIndex + 1
+            else if firstMapsToEven then 2 * secondIndex + 1
+            else 2 * secondIndex
+            end if
+        }
+
+        val translationToNewPermutationIndices =
+          chosenUniqueValues.sorted.zipWithIndex.toMap
+
+        chosenUniqueValues.map(translationToNewPermutationIndices.apply)
+      end breed
+
+      override def initialChromosome: Chromosome =
+        (0 until startingPhenotype.size).toVector
+
+      override def phenotype(chromosome: Chromosome): Phenotype =
+        chromosome.map(startingPhenotype.apply)
+
+end EvolutionTest
+
 class EvolutionTest:
-  // TODO - suppose we start with an initial chromosome that is the optimal one?
-  // Need a test for that...
+  @TestFactory
+  def evolutionSticksWithTheSortedSequence(): DynamicTests =
+    ascendingValueSequences
+      .withLimit(1000)
+      .dynamicTests { (targetSortedSequence: Phenotype) =>
+
+        given Evolution[Chromosome, Phenotype] =
+          evolution(startingPhenotype = targetSortedSequence)
+
+        end given
+
+        val fittest: Phenotype =
+          Evolution.of(
+            maximumNumberOfRetries = 1,
+            maximumPopulationSize = 200
+          )
+
+        assert(
+          fittest == targetSortedSequence
+        )
+      }
+  end evolutionSticksWithTheSortedSequence
+
   @TestFactory
   def evolutionSelectsTheSortedSequence(): DynamicTests =
-    type Phenotype = Vector[Long]
-
-    val ascendingValueSequences: Trials[Phenotype] =
-      trialsApi
-        .choose(0L until 9L)
-        .several[Vector[Long]]
-        .filter(_.nonEmpty)
-        .flatMap(increments =>
-          trialsApi
-            .longs(1L, 10L)
-            .map(lowest => increments.scanLeft(lowest)(_ + _))
-        )
-
     ascendingValueSequences
       .flatMap(ascendingValueSequence =>
         trialsApi
@@ -36,78 +131,9 @@ class EvolutionTest:
       )
       .withLimit(1000)
       .dynamicTests { (targetSortedSequence: Phenotype, mess: Phenotype) =>
-        type Chromosome = Vector[Int]
 
-        given Evolution[Chromosome, Phenotype] with
-          override def mutate(chromosome: Chromosome)(using
-              random: Random
-          ): Chromosome =
-            require(1 < chromosome.size)
-
-            if random.nextBoolean() then
-              // Swap a pair of indices...
-              val indexOne = random.nextInt(chromosome.size)
-
-              val indexTwo =
-                (indexOne + 1 + random.nextInt(
-                  chromosome.size - 1
-                )) % chromosome.size
-
-              val withTwoIndicesSwapped = chromosome
-                .updated(indexOne, chromosome(indexTwo))
-                .updated(indexTwo, chromosome(indexOne))
-
-              withTwoIndicesSwapped
-            else
-              // Cyclic permutation...
-              val rotateLeftBy = 1 + random.nextInt(chromosome.size - 1)
-
-              val (prefix, suffix) = chromosome.splitAt(rotateLeftBy)
-
-              suffix ++ prefix
-            end if
-          end mutate
-
-          override def breed(
-              first: Chromosome,
-              second: Chromosome
-          )(using random: Random): Chromosome =
-            require(first.size == second.size)
-
-            val firstMapsToEven = random.nextBoolean()
-
-            val chosenUniqueValues = (first zip second).map {
-              (firstIndex, secondIndex) =>
-                val chooseTheFirst = random.nextBoolean()
-
-                if chooseTheFirst then
-                  if firstMapsToEven then 2 * firstIndex
-                  else 2 * firstIndex + 1
-                else if firstMapsToEven then 2 * secondIndex + 1
-                else 2 * secondIndex
-                end if
-            }
-
-            val translationToNewPermutationIndices =
-              chosenUniqueValues.sorted.zipWithIndex.toMap
-
-            chosenUniqueValues.map(translationToNewPermutationIndices.apply)
-          end breed
-
-          override def initialChromosome: Chromosome =
-            (0 until mess.size).toVector
-
-          override def phenotype(chromosome: Chromosome): Phenotype =
-            chromosome.map(mess.apply)
-
-        end given
-
-        def numberOfCorrectlyOrderedAdjacentElements(phenotype: Phenotype) =
-          phenotype.zip(phenotype.tail).count(_ <= _)
-        end numberOfCorrectlyOrderedAdjacentElements
-
-        given Order[Phenotype] =
-          Order.by(numberOfCorrectlyOrderedAdjacentElements)
+        given Evolution[Chromosome, Phenotype] =
+          evolution(startingPhenotype = mess)
 
         val fittest: Phenotype =
           Evolution.of(
@@ -120,4 +146,5 @@ class EvolutionTest:
         )
       }
   end evolutionSelectsTheSortedSequence
+
 end EvolutionTest
