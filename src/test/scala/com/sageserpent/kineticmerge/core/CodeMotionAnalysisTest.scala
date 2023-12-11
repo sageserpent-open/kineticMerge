16e67b0abf7adf50a54477f81201264ebcff74ff
Gerard Murphy <gjmurphy1@icloud.com>
2023-12-11 10:13:46 +0000

    Back to big matches in the test cases. Performance is lousy, and matches may well be being missed out on; seeing very long overlapping matched sections that look like failures to see a slightly larger match...

--- src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
@@ -155,7 +155,7 @@ class CodeMotionAnalysisTest:
     // each other ...
     val sequences =
       trialsApi
-        .integers(1, 10)
+        .integers(1, 100)
         .flatMap(sequenceLength =>
           trialsApi
             .choose(alphabet)
