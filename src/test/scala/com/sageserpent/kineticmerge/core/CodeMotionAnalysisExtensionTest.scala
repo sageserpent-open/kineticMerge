15437b813dd41c22c61951d2c8ed318afad08b6d
Gerard Murphy <gjmurphy1@icloud.com>
2024-01-02 10:35:59 +0000

    Add the example merge from https://github.com/sageserpent-open/kineticMerge/issues/19 in simplified form to `CodeMotionAnalysisTest`. It produces a conflicted merge for the SBT sources...

--- src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisExtensionTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisExtensionTest.scala
@@ -10,22 +10,35 @@ import pprint.*
 class CodeMotionAnalysisExtensionTest extends ProseExamples:
   @Test
   def proseCanBeMerged(): Unit =
-    type FakePath = Int
+    type FakePath = String
 
-    val fakePath: FakePath = 1
+    val prosePath: FakePath    = "prose"
+    val sbtBuildPath: FakePath = "sbtBuild"
 
-    def sourcesFrom(
-        textContent: String,
-        label: String
-    ): Sources[FakePath, Token] =
+    val baseSources =
       MappedContentSources(
-        contentsByPath = Map(fakePath -> tokens(textContent).get),
-        label = label
+        contentsByPath = Map(
+          prosePath    -> tokens(wordsworth).get,
+          sbtBuildPath -> tokens(baseSbtBuild).get
+        ),
+        label = "base"
       )
-
-    val baseSources  = sourcesFrom(textContent = wordsworth, label = "base")
-    val leftSources  = sourcesFrom(textContent = jobsworth, label = "left")
-    val rightSources = sourcesFrom(textContent = emsworth, label = "right")
+    val leftSources =
+      MappedContentSources(
+        contentsByPath = Map(
+          prosePath    -> tokens(jobsworth).get,
+          sbtBuildPath -> tokens(leftSbtBuild).get
+        ),
+        label = "left"
+      )
+    val rightSources =
+      MappedContentSources(
+        contentsByPath = Map(
+          prosePath    -> tokens(emsworth).get,
+          sbtBuildPath -> tokens(rightSbtBuild).get
+        ),
+        label = "right"
+      )
 
     val Right(codeMotionAnalysis) = CodeMotionAnalysis.of(
       base = baseSources,
@@ -38,85 +51,24 @@ class CodeMotionAnalysisExtensionTest extends ProseExamples:
       hashFunction = Hashing.murmur3_32_fixed()
     ): @unchecked
 
-    val Right(
-      MergedWithConflicts(leftElements, rightElements)
-    ) = codeMotionAnalysis.mergeAt(path = fakePath)(equality =
-      Token.equality
-    ): @unchecked
+    def merge(path: FakePath): Unit =
+      val Right(
+        MergedWithConflicts(leftElements, rightElements)
+      ) =
+        codeMotionAnalysis.mergeAt(path)(equality = Token.equality): @unchecked
 
-    pprintln(leftElements.map(_.text).mkString)
-    println("*********************************")
-    pprintln(rightElements.map(_.text).mkString)
+      println(s"**** Merge at path: $path ****")
+      pprintln(leftElements.map(_.text).mkString)
+      println("*********************************")
+      pprintln(rightElements.map(_.text).mkString)
+    end merge
+
+    merge(prosePath)
+    merge(sbtBuildPath)
   end proseCanBeMerged
 end CodeMotionAnalysisExtensionTest
 
 trait ProseExamples:
-  protected val scalaStuff =
-    """
-      |package com.sageserpent.kineticmerge.core
-      |
-      |import org.junit.jupiter.api.Assertions.fail
-      |
-      |import scala.annotation.tailrec
-      |
-      |extension [Element](sequence: Seq[Element])
-      |  /* Replacement for ```should contain inOrderElementsOf```; as I'm not sure if
-      |   * that actually detects subsequences correctly in the presence of duplicates. */
-      |  def isSubsequenceOf(
-      |      anotherSequence: Seq[? >: Element]
-      |  ): Unit =
-      |    isSubsequenceOf(anotherSequence, negated = false)
-      |
-      |  def isNotSubsequenceOf(
-      |      anotherSequence: Seq[? >: Element]
-      |  ): Unit =
-      |    isSubsequenceOf(anotherSequence, negated = true)
-      |
-      |  private def isSubsequenceOf[ElementSupertype >: Element](
-      |      anotherSequence: Seq[ElementSupertype],
-      |      negated: Boolean
-      |  ): Unit =
-      |    @tailrec
-      |    def verify(
-      |        sequenceRemainder: Seq[Element],
-      |        anotherSequenceRemainder: Seq[ElementSupertype],
-      |        matchingPrefix: Seq[Element]
-      |    ): Unit =
-      |      if sequenceRemainder.isEmpty then
-      |        if negated then
-      |          fail(
-      |            s"Assertion failed because $sequence is a subsequence of $anotherSequence."
-      |          )
-      |      else if anotherSequenceRemainder.isEmpty then
-      |        if !negated then
-      |          if matchingPrefix.isEmpty then
-      |            fail(
-      |              s"Assertion failed because $sequence is not a subsequence of $anotherSequence - no prefix matches found, either."
-      |            )
-      |          else
-      |            fail(
-      |              s"Assertion failed because $sequence is not a subsequence of $anotherSequence, matched prefix $matchingPrefix but failed to find the remaining $sequenceRemainder."
-      |            )
-      |      else if sequenceRemainder.head == anotherSequenceRemainder.head then
-      |        verify(
-      |          sequenceRemainder.tail,
-      |          anotherSequenceRemainder.tail,
-      |          matchingPrefix :+ sequenceRemainder.head
-      |        )
-      |      else
-      |        verify(
-      |          sequenceRemainder,
-      |          anotherSequenceRemainder.tail,
-      |          matchingPrefix
-      |        )
-      |      end if
-      |    end verify
-      |
-      |    verify(sequence, anotherSequence, sequence.empty)
-      |  end isSubsequenceOf
-      |end extension
-      |""".stripMargin
-
   protected val wordsworth =
     """
       |I wandered lonely as a cloud
@@ -202,5 +154,261 @@ trait ProseExamples:
       |Which is the bliss of solitude;
       |And then my heart with pleasure fills,
       |And sashays with the fishing boats.
+      |""".stripMargin
+
+  protected val baseSbtBuild =
+    """
+      |import scala.sys.process.*
+      |import scala.language.postfixOps
+      |import sbtrelease.ReleaseStateTransformations.*
+      |import xerial.sbt.Sonatype.*
+      |
+      |lazy val javaVersion = "14"
+      |
+      |ThisBuild / version := "0.1.0-SNAPSHOT"
+      |
+      |ThisBuild / scalaVersion := "3.3.0"
+      |
+      |ThisBuild / javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion)
+      |
+      |lazy val packageExecutable =
+      |  taskKey[String]("Package an executable with Coursier")
+      |
+      |lazy val root = (project in file("."))
+      |  .settings(
+      |    publishTo              := sonatypePublishToBundle.value,
+      |    pomIncludeRepository   := { _ => false },
+      |    sonatypeCredentialHost := "s01.oss.sonatype.org",
+      |    publishMavenStyle      := true,
+      |    licenses += ("MIT", url("https://opensource.org/licenses/MIT")),
+      |    organization     := "com.sageserpent",
+      |    organizationName := "sageserpent",
+      |    description := "Merge branches in the presence of code motion within and between files.",
+      |    sonatypeProjectHosting := Some(
+      |      GitHubHosting(
+      |        user = "sageserpent-open",
+      |        repository = "kineticMerge",
+      |        email = "gjmurphy1@icloud.com"
+      |      )
+      |    ),
+      |    releaseCrossBuild := false, // No cross-building here - just Scala 3.
+      |    releaseProcess := Seq[ReleaseStep](
+      |      checkSnapshotDependencies,
+      |      inquireVersions,
+      |      runClean,
+      |      runTest,
+      |      setReleaseVersion,
+      |      commitReleaseVersion,
+      |      tagRelease,
+      |      releaseStepCommandAndRemaining(
+      |        "publishSigned"
+      |      ), // ... finally the publishing step using SBT's own mechanism.
+      |      releaseStepCommand("sonatypeBundleRelease"),
+      |      releaseStepCommand("packageExecutable"),
+      |      setNextVersion,
+      |      commitNextVersion,
+      |      pushChanges
+      |    ),
+      |    scalacOptions ++= List("-source:future"),
+      |    name := "kinetic-merge",
+      |    packageExecutable := {
+      |      val _ = publishLocal.value; (rabinFingerprint / publishLocal).value
+      |
+      |      val localArtifactCoordinates =
+      |        s"${organization.value}:${name.value}_${scalaBinaryVersion.value}:${version.value}"
+      |
+      |      val executablePath = s"${target.value}${Path.sep}${name.value}"
+      |
+      |      s"cs bootstrap --verbose --bat=true --scala-version ${scalaBinaryVersion.value} -f $localArtifactCoordinates -o $executablePath" !
+      |
+      |      name.value
+      |    })
+      |""".stripMargin
+
+  protected val leftSbtBuild =
+    """
+      |import scala.sys.process.*
+      |import scala.language.postfixOps
+      |import sbtrelease.ReleaseStateTransformations.*
+      |import xerial.sbt.Sonatype.*
+      |import scala.xml.transform.{RuleTransformer, RewriteRule}
+      |import scala.xml.{Node, Elem}
+      |
+      |enablePlugins(ShadingPlugin)
+      |
+      |lazy val javaVersion = "14"
+      |
+      |ThisBuild / version := "0.1.0-SNAPSHOT"
+      |
+      |ThisBuild / scalaVersion := "3.3.0"
+      |
+      |ThisBuild / javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion)
+      |
+      |lazy val packageExecutable =
+      |  taskKey[String]("Package an executable with Coursier")
+      |
+      |lazy val versionResource =
+      |  settingKey[File]("Location of generated version resource file.")
+      |
+      |lazy val root = (project in file("."))
+      |  .settings(
+      |    publishTo              := sonatypePublishToBundle.value,
+      |    pomIncludeRepository   := { _ => false },
+      |    sonatypeCredentialHost := "s01.oss.sonatype.org",
+      |    publishMavenStyle      := true,
+      |    pomPostProcess := { node =>
+      |      val rejectedDependencyArtifact =
+      |        (rabinFingerprint / Compile / packageBin / artifact).value.name
+      |
+      |      object removeRejectedDependencyArtifact extends RewriteRule {
+      |        override def transform(subtreeNode: Node): Seq[Node] =
+      |          subtreeNode match {
+      |            case element: Elem if element.label == "dependency" =>
+      |              if (
+      |                element.child
+      |                  .filter(_.label == "artifactId")
+      |                  .exists(rejectedDependencyArtifact == _.text)
+      |              ) Seq.empty
+      |              else Seq(subtreeNode)
+      |            case _ => Seq(subtreeNode)
+      |          }
+      |      }
+      |
+      |      val transformer = new RuleTransformer(removeRejectedDependencyArtifact)
+      |
+      |      transformer.transform(node).head
+      |    },
+      |    licenses += ("MIT", url("https://opensource.org/licenses/MIT")),
+      |    organization     := "com.sageserpent",
+      |    organizationName := "sageserpent",
+      |    description := "Merge branches in the presence of code motion within and between files.",
+      |    sonatypeProjectHosting := Some(
+      |      GitHubHosting(
+      |        user = "sageserpent-open",
+      |        repository = "kineticMerge",
+      |        email = "gjmurphy1@icloud.com"
+      |      )
+      |    ),
+      |    releaseCrossBuild := false, // No cross-building here - just Scala 3.
+      |    releaseProcess := Seq[ReleaseStep](
+      |      checkSnapshotDependencies,
+      |      inquireVersions,
+      |      runClean,
+      |      runTest,
+      |      setReleaseVersion,
+      |      commitReleaseVersion,
+      |      tagRelease,
+      |      releaseStepCommand("packageExecutable"),
+      |      releaseStepCommand(
+      |        "publishSigned"
+      |      ), // ... finally the publishing step using SBT's own mechanism.
+      |      releaseStepCommand("sonatypeBundleRelease"),
+      |      setNextVersion,
+      |      commitNextVersion,
+      |      pushChanges
+      |    ),
+      |    scalacOptions ++= List("-source:future"),
+      |    name := "kinetic-merge",
+      |    versionResource := {
+      |      val additionalResourcesDirectory = (Compile / resourceManaged).value
+      |
+      |      additionalResourcesDirectory.toPath.resolve("version.txt").toFile
+      |    },
+      |    Compile / resourceGenerators += Def.task {
+      |      val location = versionResource.value
+      |
+      |      val packagingVersion = (ThisBuild / version).value
+      |
+      |      println(
+      |        s"Generating version resource: $location for version: $packagingVersion"
+      |      )
+      |
+      |      IO.write(location, packagingVersion)
+      |
+      |      Seq(location)
+      |    }.taskValue,
+      |    packageExecutable := {
+      |      val packagingVersion = (ThisBuild / version).value
+      |
+      |      println(s"Packaging executable with version: $packagingVersion")
+      |
+      |      val localArtifactCoordinates =
+      |        s"${organization.value}:${name.value}_${scalaBinaryVersion.value}:$packagingVersion"
+      |
+      |      val executablePath = s"${target.value}${Path.sep}${name.value}"
+      |
+      |      s"cs bootstrap --verbose --bat=true --scala-version ${scalaBinaryVersion.value} -f $localArtifactCoordinates -o $executablePath" !
+      |
+      |      name.value
+      |    })
+      |""".stripMargin
+
+  protected val rightSbtBuild =
+    """
+      |import scala.sys.process.*
+      |import scala.language.postfixOps
+      |import sbtrelease.ReleaseStateTransformations.*
+      |import xerial.sbt.Sonatype.*
+      |
+      |lazy val javaVersion = "14"
+      |
+      |ThisBuild / version := "0.1.0-SNAPSHOT"
+      |
+      |ThisBuild / scalaVersion := "3.3.0"
+      |
+      |ThisBuild / javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion)
+      |
+      |lazy val packageExecutable =
+      |  taskKey[String]("Package an executable with Coursier")
+      |
+      |lazy val root = (project in file("."))
+      |  .settings(
+      |    publishTo              := sonatypePublishToBundle.value,
+      |    pomIncludeRepository   := { _ => false },
+      |    sonatypeCredentialHost := "s01.oss.sonatype.org",
+      |    publishMavenStyle      := true,
+      |    licenses += ("MIT", url("https://opensource.org/licenses/MIT")),
+      |    organization     := "com.sageserpent",
+      |    organizationName := "sageserpent",
+      |    description := "Merge branches in the presence of code motion within and between files.",
+      |    sonatypeProjectHosting := Some(
+      |      GitHubHosting(
+      |        user = "sageserpent-open",
+      |        repository = "kineticMerge",
+      |        email = "gjmurphy1@icloud.com"
+      |      )
+      |    ),
+      |    releaseCrossBuild := false, // No cross-building here - just Scala 3.
+      |    releaseProcess := Seq[ReleaseStep](
+      |      checkSnapshotDependencies,
+      |      inquireVersions,
+      |      runClean,
+      |      runTest,
+      |      setReleaseVersion,
+      |      commitReleaseVersion,
+      |      tagRelease,
+      |      releaseStepCommandAndRemaining(
+      |        "publishSigned"
+      |      ), // ... finally the publishing step using SBT's own mechanism.
+      |      releaseStepCommand("sonatypeBundleRelease"),
+      |      releaseStepCommand("packageExecutable"),
+      |      setNextVersion,
+      |      commitNextVersion,
+      |      pushChanges
+      |    ),
+      |    scalacOptions ++= List("-source:future"),
+      |    name := "kinetic-merge",
+      |    packageExecutable := {
+      |      val _ = publishLocal.value
+      |
+      |      val localArtifactCoordinates =
+      |        s"${organization.value}:${name.value}_${scalaBinaryVersion.value}:${version.value}"
+      |
+      |      val executablePath = s"${target.value}${Path.sep}${name.value}"
+      |
+      |      s"cs bootstrap --verbose --bat=true --scala-version ${scalaBinaryVersion.value} -f $localArtifactCoordinates -o $executablePath" !
+      |
+      |      name.value
+      |    })
       |""".stripMargin
 end ProseExamples
