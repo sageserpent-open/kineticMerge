b233867b9bf7a8076aaa504048021e0214b93e1d
Gerard Murphy <gjmurphy1@icloud.com>
2024-01-27 13:03:52 +0000

    Finesse the check for overlapping sections in `MappedContentSources.filesByPathUtilising` so that if one section is *subsumed* by another, this broken out as a precondition failure due to the calling code being incorrect rather than as an admissible failure due to ambiguous matches.

--- src/main/scala/com/sageserpent/kineticmerge/core/MappedContentSources.scala
+++ src/main/scala/com/sageserpent/kineticmerge/core/MappedContentSources.scala
@@ -1,12 +1,12 @@
 package com.sageserpent.kineticmerge.core
 
 case class MappedContentSources[Path, Element](
-                        contentsByPath: Map[Path, IndexedSeq[Element]],
-                        label: String
-                      ) extends Sources[Path, Element]:
+    contentsByPath: Map[Path, IndexedSeq[Element]],
+    label: String
+) extends Sources[Path, Element]:
   override def filesByPathUtilising(
-                                     mandatorySections: Set[Section[Element]]
-                                   ): Map[Path, File[Element]] =
+      mandatorySections: Set[Section[Element]]
+  ): Map[Path, File[Element]] =
     val sectionsByPath = mandatorySections.groupBy(pathFor)
 
     contentsByPath.map { case (path, content) =>
@@ -15,12 +15,20 @@ case class MappedContentSources[Path, Element](
       path -> File(
         if pertinentSections.nonEmpty then
           val sectionsInStartOffsetOrder =
-            pertinentSections.toSeq.sortBy(_.startOffset)
+            // Sort by the lowest start offset and then use the largest one past
+            // end offset as a tiebreaker.
+            pertinentSections.toSeq.sortBy(section =>
+              section.startOffset -> -section.onePastEndOffset
+            )
 
           sectionsInStartOffsetOrder
             .zip(sectionsInStartOffsetOrder.tail)
             .foreach((first, second) =>
               if first.onePastEndOffset > second.startOffset then
+                require(
+                  first.onePastEndOffset < second.onePastEndOffset,
+                  s"Section $second is subsumed by section $first."
+                )
                 throw new OverlappingSections(path, first, second)
             )
 
@@ -30,11 +38,11 @@ case class MappedContentSources[Path, Element](
             ) { case ((onePastLastEndOffset, partialResult), section) =>
               section.onePastEndOffset ->
                 ((if onePastLastEndOffset < section.startOffset then
-                  partialResult :+ this.section(path)(
-                    onePastLastEndOffset,
-                    section.startOffset - onePastLastEndOffset
-                  )
-                else partialResult) :+ section)
+                    partialResult :+ this.section(path)(
+                      onePastLastEndOffset,
+                      section.startOffset - onePastLastEndOffset
+                    )
+                  else partialResult) :+ section)
             }
 
           if content.size > onePastLastEndOffset then
@@ -56,43 +64,41 @@ case class MappedContentSources[Path, Element](
     }
   end filesByPathUtilising
 
-  override def section(path: Path)(
-    startOffset: Int,
-    size: Int
-  ): Section[Element] = SectionImplementation(path, startOffset, size)
-
   override def pathFor(section: Section[Element]): Path =
     section match
       // If the section implementation does not come from this `FakeSources`,
       // then it can't be accepted, it's up to the client to be consistent.
-      case SectionImplementation(path, _, _)
-        if contentsByPath.contains(path) =>
+      case SectionImplementation(path, _, _) if contentsByPath.contains(path) =>
         path
+
+  override def section(path: Path)(
+      startOffset: Int,
+      size: Int
+  ): Section[Element] = SectionImplementation(path, startOffset, size)
 
   override def paths: Set[Path] = contentsByPath.keySet
 
   class OverlappingSections(
-                             path: Path,
-                             first: Section[Element],
-                             second: Section[Element]
-                           ) extends RuntimeException({
-    val overlap = section(path)(
-      startOffset = second.startOffset,
-      size = first.onePastEndOffset - second.startOffset
-    )
+      path: Path,
+      first: Section[Element],
+      second: Section[Element]
+  ) extends RuntimeException({
+        val overlap = section(path)(
+          startOffset = second.startOffset,
+          size = first.onePastEndOffset - second.startOffset
+        )
 
-    s"Overlapping section detected on side: $label at path: $path: $first (content: ${first.content}) overlaps with start of section: $second (content: ${second.content}), overlap content: ${overlap.content}."
-  }):
+        s"Overlapping section detected on side: $label at path: $path: $first (content: ${first.content}) overlaps with start of section: $second (content: ${second.content}), overlap content: ${overlap.content}."
+      }):
 
   end OverlappingSections
 
   case class SectionImplementation(
-                                    path: Path,
-                                    override val startOffset: Int,
-                                    override val size: Int
-                                  ) extends Section[Element]:
+      path: Path,
+      override val startOffset: Int,
+      override val size: Int
+  ) extends Section[Element]:
     override def content: IndexedSeq[Element] =
       contentsByPath(path).slice(startOffset, onePastEndOffset)
   end SectionImplementation
 end MappedContentSources
-
