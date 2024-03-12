300d99155836d7d9b86c76c30a65b6e2539f17a4
Gerard Murphy <gjmurphy1@icloud.com>
2024-03-12 15:54:24 +0000

    Flesh out the first test in `MergeResultDetectingMotionTest`. It fails, as expected. Not happy with how the first assertion succeeds with just a pass-through implementation, need to finesse the test and SUT...

--- src/test/scala/com/sageserpent/kineticmerge/core/MergeResultDetectingMotionTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/MergeResultDetectingMotionTest.scala
@@ -1,16 +1,37 @@
 package com.sageserpent.kineticmerge.core
 
 import com.sageserpent.kineticmerge.core.ExpectyFlavouredAssert.assert
+import com.sageserpent.kineticmerge.core.MergeResultDetectingMotionTest.Element
 import org.junit.jupiter.api.Test
+
+object MergeResultDetectingMotionTest:
+  type Element = Int
+end MergeResultDetectingMotionTest
 
 class MergeResultDetectingMotionTest:
   @Test
   def baseAndLeftMatchWhenMergeDoesNotAlignTheTwoMatchingSections: Unit =
-    // Test plan:
-    // 1. Set up base and left match.
-    // 2. Apply coincident deletion.
-    // 3. Expect the core merge result to be a left deletion.
-    // 4. Expect an edit of the left section to nothing.
+    val baseElement: Element = 1
+    val leftElement: Element = 2
 
-    assert(true)
+    val baseAndLeft = Match.BaseAndLeft(baseElement, leftElement)
+
+    val matchesByElement =
+      Map(baseElement -> baseAndLeft, leftElement -> baseAndLeft)
+
+    def matchesFor(element: Element): Set[Match[Element]] = matchesByElement
+      .get(element)
+      .fold(ifEmpty = Set.empty[Match[Element]])(Set(_))
+
+    val mergeAlgebra = MergeResultDetectingMotion.mergeAlgebra(matchesFor)
+
+    val mergeResult =
+      mergeAlgebra.coincidentDeletion(mergeAlgebra.empty, baseElement)
+
+    assert(
+      FullyMerged(elements = IndexedSeq.empty) == mergeResult.coreMergeResult
+    )
+
+    assert(None == mergeResult.changesPropagatedThroughMotion(leftElement))
+  end baseAndLeftMatchWhenMergeDoesNotAlignTheTwoMatchingSections
 end MergeResultDetectingMotionTest
