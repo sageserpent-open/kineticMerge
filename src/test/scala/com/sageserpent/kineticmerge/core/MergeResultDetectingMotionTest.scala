ea4ecf91d510606b9cbdb5abc9eb054e305921f3
Gerard Murphy <gjmurphy1@icloud.com>
2024-03-13 18:00:51 +0000

    Swap out use of Mockito in `MergeResultDetectingMotionTest` for an audit-based approach more in line with the way the SUT works.

--- src/test/scala/com/sageserpent/kineticmerge/core/MergeResultDetectingMotionTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/MergeResultDetectingMotionTest.scala
@@ -3,26 +3,89 @@ package com.sageserpent.kineticmerge.core
 import com.sageserpent.americium.Trials
 import com.sageserpent.americium.junit5.{DynamicTests, given}
 import com.sageserpent.kineticmerge.core.ExpectyFlavouredAssert.assert
-import com.sageserpent.kineticmerge.core.MergeResultDetectingMotionTest.{Element, mockCoreMergeAlgebra}
+import com.sageserpent.kineticmerge.core.MergeResultDetectingMotionTest.Operation.*
+import com.sageserpent.kineticmerge.core.MergeResultDetectingMotionTest.{Element, auditingCoreMergeAlgebra}
 import org.junit.jupiter.api.TestFactory
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{RETURNS_SELF, mock, verify}
-import org.mockito.junit.jupiter.MockitoExtension
 
 object MergeResultDetectingMotionTest:
-  type Element = Int
+  type Element  = Int
+  type Audit[X] = Vector[Operation[X]]
 
-  // NOTE: yes, this is imperative mocking in a pure-functional context, but try
-  // writing an auditing merge algebra and getting it to plug into
-  // `MergeResultDetectingMotion`. Then cut over the test suite and decide
-  // whether it was worth it...
-  def mockCoreMergeAlgebra() =
-    mock(classOf[merge.MergeAlgebra[MergeResult, Element]], RETURNS_SELF)
+  enum Operation[X]:
+    case Preservation(preserved: X)
+    case LeftInsertion(inserted: X)
+    case RightInsertion(inserted: X)
+    case CoincidentInsertion(inserted: X)
+    case LeftDeletion(deleted: X)
+    case RightDeletion(deleted: X)
+    case CoincidentDeletion(deleted: X)
+    case LeftEdit(edited: X, edits: IndexedSeq[X])
+    case RightEdit(edited: X, edits: IndexedSeq[X])
+    case CoincidentEdit(edited: X, edits: IndexedSeq[X])
+    case Conflict(
+        edited: IndexedSeq[X],
+        leftEdits: IndexedSeq[X],
+        rightEdits: IndexedSeq[X]
+    )
+
+  end Operation
+
+  object auditingCoreMergeAlgebra extends merge.MergeAlgebra[Audit, Element]:
+    override def empty: Audit[Element] = Vector.empty
+    override def preservation(
+        result: Audit[Element],
+        preservedElement: Element
+    ): Audit[Element] = result :+ Preservation(preservedElement)
+    override def leftInsertion(
+        result: Audit[Element],
+        insertedElement: Element
+    ): Audit[Element] = result :+ LeftInsertion(insertedElement)
+    override def rightInsertion(
+        result: Audit[Element],
+        insertedElement: Element
+    ): Audit[Element] = result :+ RightInsertion(insertedElement)
+    override def coincidentInsertion(
+        result: Audit[Element],
+        insertedElement: Element
+    ): Audit[Element] = result :+ CoincidentInsertion(insertedElement)
+    override def leftDeletion(
+        result: Audit[Element],
+        deletedElement: Element
+    ): Audit[Element] = result :+ LeftDeletion(deletedElement)
+    override def rightDeletion(
+        result: Audit[Element],
+        deletedElement: Element
+    ): Audit[Element] = result :+ RightDeletion(deletedElement)
+    override def coincidentDeletion(
+        result: Audit[Element],
+        deletedElement: Element
+    ): Audit[Element] = result :+ CoincidentDeletion(deletedElement)
+    override def leftEdit(
+        result: Audit[Element],
+        editedElement: Element,
+        editElements: IndexedSeq[Element]
+    ): Audit[Element] = result :+ LeftEdit(editedElement, editElements)
+    override def rightEdit(
+        result: Audit[Element],
+        editedElement: Element,
+        editElements: IndexedSeq[Element]
+    ): Audit[Element] = result :+ RightEdit(editedElement, editElements)
+    override def coincidentEdit(
+        result: Audit[Element],
+        editedElement: Element,
+        editElements: IndexedSeq[Element]
+    ): Audit[Element] = result :+ CoincidentEdit(editedElement, editElements)
+    override def conflict(
+        result: Audit[Element],
+        editedElements: IndexedSeq[Element],
+        leftEditElements: IndexedSeq[Element],
+        rightEditElements: IndexedSeq[Element]
+    ): Audit[Element] =
+      result :+ Conflict(editedElements, leftEditElements, rightEditElements)
+  end auditingCoreMergeAlgebra
+
 end MergeResultDetectingMotionTest
 
-@ExtendWith(Array(classOf[MockitoExtension]))
 class MergeResultDetectingMotionTest:
   @TestFactory
   def pairwiseMatchWhenMergeDoesNotAlignTheTwoMatchingSections: DynamicTests =
@@ -54,17 +117,15 @@ class MergeResultDetectingMotionTest:
         .get(element)
         .fold(ifEmpty = Set.empty[Match[Element]])(Set(_))
 
-      val coreMergeAlgebra = mockCoreMergeAlgebra()
-
       val mergeAlgebra =
-        MergeResultDetectingMotion.mergeAlgebra(matchesFor, coreMergeAlgebra)
+        MergeResultDetectingMotion
+          .mergeAlgebra(matchesFor, auditingCoreMergeAlgebra)
 
       val mergeResult =
         mergeAlgebra.coincidentDeletion(mergeAlgebra.empty, baseElement)
 
-      verify(coreMergeAlgebra).coincidentDeletion(
-        any(),
-        ArgumentMatchers.eq(baseElement)
+      assert(
+        Vector(CoincidentDeletion(baseElement)) == mergeResult.coreMergeResult
       )
 
       assert(None == mergeResult.changesPropagatedThroughMotion(ourSideElement))
