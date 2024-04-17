4b23be3eafdfa6aba8e0d3bebf800b8615e37c5e
Gerard Murphy <gjmurphy1@icloud.com>
2024-04-17 11:14:57 +0100

    Correct obsolete commentary.

--- src/main/scala/com/sageserpent/kineticmerge/ProgressRecording.scala
+++ src/main/scala/com/sageserpent/kineticmerge/ProgressRecording.scala
@@ -5,14 +5,12 @@ import org.jline.utils.WriterOutputStream
 
 import java.io.PrintStream
 import java.nio.charset.Charset
-
 import java.time.Duration as JavaDuration
 
 /** Records progress from zero up to some implied maximum set up by
-  * [[ProgressRecording.newSession]]. It is a ratchet, so attempting to decrease
-  * the recorded progress is ignored. Attempting to record more than the maximum
-  * simply records the maximum. <p/> Closing the session implicity records the
-  * maximum amount of progress.
+  * [[ProgressRecording.newSession]]. Progress may be moved up or down; so it is
+  * possible to use this to count progress up from zero to the maximum, or to
+  * count progress down from the maximum to zero.
   */
 trait ProgressRecordingSession extends AutoCloseable:
   /** @param amount
@@ -24,19 +22,25 @@ end ProgressRecordingSession
 trait ProgressRecording:
   /** Create a new progress recording session that has zero recorded progress.
     *
+    * @param label
+    *   Labels a specific activity whose progress is being recorded.
     * @param maximumProgress
     *   The maximum amount of progress that can be recorded.
+    * @param initialProgress
+    *   This may be anywhere between zero and {@code maximumProgress}.
     * @return
-    *   An instance of [[ProgressRecordingSession]] that has zero progress
+    *   An instance of [[ProgressRecordingSession]] with {@code initialProgress}
     *   recorded.
     */
   def newSession(label: String, maximumProgress: Int)(
-      initialProgress: Int): ProgressRecordingSession
+      initialProgress: Int
+  ): ProgressRecordingSession
 end ProgressRecording
 
 object NoProgressRecording extends ProgressRecording:
   override def newSession(label: String, maximumProgress: Int)(
-      initialProgress: Int): ProgressRecordingSession =
+      initialProgress: Int
+  ): ProgressRecordingSession =
     Session
 
   private object Session extends ProgressRecordingSession:
@@ -46,9 +50,15 @@ object NoProgressRecording extends ProgressRecording:
   end Session
 end NoProgressRecording
 
+/** An instance of [[ProgressRecordingSession]] created by
+  * [[ConsoleProgressRecording.newSession]] displays a progress bar on the
+  * console, if one is available to the process. Closing such an instance
+  * removes the progress bar.
+  */
 object ConsoleProgressRecording extends ProgressRecording:
   override def newSession(label: String, maximumProgress: Int)(
-      initialProgress: Int): ProgressRecordingSession =
+      initialProgress: Int
+  ): ProgressRecordingSession =
     new ProgressRecordingSession:
       private val progressBar = Option(System.console()).map(console =>
         val progressBarConsumer = new ConsoleProgressBarConsumer(
