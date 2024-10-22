af774510ad44a859bd28ca6b7a6549677fea9507
Gerard Murphy <gjmurphy1@icloud.com>
2024-10-22 13:57:18 +0100

    Minimized!

--- src/main/scala/com/sageserpent/kineticmerge/core/Bystander.scala
+++ src/main/scala/com/sageserpent/kineticmerge/core/Bystander.scala
@@ -7,28 +7,28 @@ case class Bystander():
 end Bystander
 
 object Bystander:
-  type PartialResultKey = Int
-
-  type PartialResultsCache =
-    Map[PartialResultKey, Bystander]
+  type PartialResultsCache = Unit
 
   type EvalWithPartialResultState[X] = StateT[Eval, PartialResultsCache, X]
 
-  def _of(
-           index: Int
-         ): EvalWithPartialResultState[Bystander] = ???
-
   def of(
-          index: Int
-        ): EvalWithPartialResultState[Bystander] =
+      index: Int
+  ): EvalWithPartialResultState[Bystander] =
     StateT.get.flatMap { partialResultsCache =>
       _of(index)
         .flatMap(computedResult =>
           StateT
-            .set(
-              partialResultsCache + (index -> computedResult)
-            ) >> StateT.pure(computedResult)
+            .set(???)
+          // This last call to `>>` with the rhs operand triggers the assertion
+          // failure. Commenting it out yields an expected compilation error...
+            >> StateT.pure(
+              computedResult
+            )
         )
     }
   end of
+
+  def _of(
+      index: Int
+  ): EvalWithPartialResultState[Bystander] = ???
 end Bystander
