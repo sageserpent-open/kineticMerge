2cba2de977dcd1c5c205316052e02f3dc98d085b
Gerard Murphy <gjmurphy1@icloud.com>
2025-02-14 14:12:22 +0000

    Manually shrinking `CodeMotionAnalysisTest.overlappingSmallerAllSidesMatchesCanEatIntoALargerPairwiseMatchWithoutLeavingAnyFragmentsVariation`, still getting a failure...

--- src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
+++ src/test/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysisTest.scala
@@ -1494,9 +1494,7 @@ class CodeMotionAnalysisTest:
     val baseSources = new FakeSources(
       Map(
         1 -> Vector(
-          5, 4, 6, 7, 8, 9, 8, 3, 2, 7, 1, 3, 1, 7, 4, 5, 4, 7, 5, 9, 6, 10, 3,
-          6, 5, 8, 5, 8, 2, 1, 5, 9, 4, 2, 7, 8, 8, 4, 3, 5, 5, 6, 10, 9, 7, 8,
-          8
+          5, 4, 7, 5, 9
         )
       ),
       "base"
@@ -1506,8 +1504,7 @@ class CodeMotionAnalysisTest:
       Map(
         1 -> Vector(
           6, 1, 2, 9, 4, 7, 7, 7, 4, 2, 8, 8, 9, 2, 7, 5, 2, 4, 8, 8, 7, 4, 7,
-          6, 9, 7, 8, 8, 3, 2, 8, 7, 2, 2, 7, 6, 9, 3, 3, 9, 4, 5, 7, 3, 7, 9,
-          8, 1, 8, 8, 4, 2, 1, 6, 7, 5
+          6, 7, 5
         )
       ),
       "left"
@@ -1515,8 +1512,7 @@ class CodeMotionAnalysisTest:
 
     val rightSources = new FakeSources(
       Map(
-        1 -> Vector(8, 5, 2, 7, 5, 7, 4, 7, 5, 4, 4, 6, 10, 5, 7, 8, 6, 2, 6, 5,
-          8, 5, 6, 2)
+        1 -> Vector(2, 7, 5, 7, 4, 7, 5, 4)
       ),
       "right"
     ) with SourcesContracts[Path, Element]
