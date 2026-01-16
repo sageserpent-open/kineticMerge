f4a9e22b18c85492c89eb03f0585576d581a7715
Gerard Murphy <gjmurphy1@icloud.com>
2026-01-16 09:57:17 +0000

    When retrieving sections from a finger-tree as part of looking for subsuming matches, de-duplicate the sections first prior to mapping to matches.

--- src/main/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysis.scala
+++ src/main/scala/com/sageserpent/kineticmerge/core/CodeMotionAnalysis.scala
@@ -473,10 +473,10 @@ object CodeMotionAnalysis extends StrictLogging:
           .get(side.pathFor(section))
           .fold(ifEmpty = Set.empty)(
             _.filterIncludes(section.closedOpenInterval)
-              .flatMap(sectionsAndTheirMatches.get)
               // NOTE: convert to a set at this point as we expect sections to
               // be duplicated when involved in ambiguous matches.
               .toSet
+              .flatMap(sectionsAndTheirMatches.get)
           )
 
       // NOTE: this is partially applied in the class so that it means: "is
